c538f70b02591754a915cd49eae66f086b7b1dc8
yasirmaxstyle <yasirmu77@gmail.com>
2025-05-09 04:08:53 +0700

    feat: add scroll to top button

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,11 +4,40 @@ async function fetchData() {
     try {
         const response = await fetch(baseUrl)
         if (!response.ok) throw new Error(response.statusText);
-        const { results } = await response.json();
+        const { results, info } = await response.json();
         const data = []
         results.forEach(element => {
             data[data.length] = { image: element.image, name: element.name }
         });
+
+        const page2 = await fetch(info.next)
+        if (!page2.ok) throw new Error(page2.statusText);
+        const { results: results2, info: info2 } = await page2.json();
+        results2.forEach(element => {
+            data[data.length] = { image: element.image, name: element.name }
+        });
+
+        const page3 = await fetch(info2.next)
+        if (!page3.ok) throw new Error(page3.statusText);
+        const { results: results3, info: info3 } = await page3.json();
+        results3.forEach(element => {
+            data[data.length] = { image: element.image, name: element.name }
+        });
+
+        const page4 = await fetch(info3.next)
+        if (!page4.ok) throw new Error(page4.statusText);
+        const { results: results4, info: info4 } = await page4.json();
+        results4.forEach(element => {
+            data[data.length] = { image: element.image, name: element.name }
+        });
+
+        const page5 = await fetch(info4.next)
+        if (!page5.ok) throw new Error(page3.statusText);
+        const { results: results5, info: info5 } = await page5.json();
+        results5.forEach(element => {
+            data[data.length] = { image: element.image, name: element.name }
+        });
+
 
         const container = document.getElementById('container-grid')
         data.forEach(element => {
@@ -39,6 +68,25 @@ async function fetchData() {
         }
 
         search.addEventListener('keyup', searchChar)
+
+        const arrow = document.querySelector('.arrow')
+
+        window.onscroll = function () { scrollFunction() };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                arrow.style.display = "block";
+            } else {
+                arrow.style.display = "none";
+            }
+        }
+
+        arrow.addEventListener('click', topFunction)
+
+        function topFunction() {
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
 
 
     } catch (error) {
