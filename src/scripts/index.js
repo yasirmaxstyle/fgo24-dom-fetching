89d105e6a8092d5fab80c3962913ed4cb00fcf98
yasirmaxstyle <yasirmu77@gmail.com>
2025-05-09 03:07:16 +0700

    feat: add program for filtering searched item in search bar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,7 +10,7 @@ async function fetchData() {
             data[data.length] = { image: element.image, name: element.name }
         });
 
-        const container = document.getElementById('container')
+        const container = document.getElementById('container-grid')
         data.forEach(element => {
             const img = document.createElement('img')
             const name = document.createElement('p')
@@ -19,8 +19,28 @@ async function fetchData() {
             img.alt = element.name
             name.textContent = element.name
             div.append(img, name)
+            div.classList.add('char-container')
             container.append(div)
+
         })
+
+        const search = document.querySelector('#search')
+        const charList = document.querySelectorAll('.char-container')
+
+        function searchChar(event) {
+            const searchCharName = event.target.value.toLowerCase()
+
+            charList.forEach(char => {
+                const singleChar = char.children[1] //children [0]: img, [1]: <p>
+                const charName = singleChar.textContent.toLowerCase() //content of <p>
+
+                charName.includes(searchCharName) ? char.style.display = 'flex' : char.style.display = 'none' //display the container if match search value, hide if not
+            })
+        }
+
+        search.addEventListener('keyup', searchChar)
+
+
     } catch (error) {
         if (error instanceof Error) console.log(error.message)
     }
